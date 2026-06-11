d95cc25f13fdcbc52d611cdc51573e4676485aac
TheDogHusky <contact@classydev.fr>
2026-06-11 23:08:47 +0200

    fix: music flow being under stuff

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@
             :options="{
                 autoplay: true
             }"
-            style="position: fixed;"
+            style="position: fixed;z-index:1000;"
         />
         <Header />
         <slot />
